f238bb0c44ceb023e03d4a2eaeaeed0c9cc2b417
petterwaw <piotr19.02@wp.pl>
2026-09-16 16:11:20 +0200

    update Docker files, init should work with docker compose up

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -9,6 +9,6 @@ async function bootstrap() {
   app.useGlobalInterceptors(new TransfromInterceptor())
   app.useGlobalFilters(new HttpExceptionFilter())
   app.enableCors();
-  await app.listen(process.env.PORT ?? 3001);
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
